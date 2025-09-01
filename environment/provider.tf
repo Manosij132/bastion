148f87d1a4c064e3da50ae97d73012b817d206f6
Manosij132 <ankur.majumder96@gmail.com>
2025-09-01 07:39:54 +0530

    added code

---
+++ environment/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.42.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name = "rg-manotf"
+    storage_account_name = "tfstatemano"
+    container_name = "tfstate"
+    key = "dev.tfstate"
+    
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "972b29e5-b4b2-4f43-b814-02879737840d"
+}
